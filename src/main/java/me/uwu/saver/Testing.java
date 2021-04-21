9f18a03f18be2891029fed401e20561ed01f99d3
UwU <mmimi0593@gmail.com>
2021-04-21 15:48:35 +0200

    Sa march bi1

--- src/main/java/me/uwu/saver/Testing.java
+++ src/main/java/me/uwu/saver/Testing.java
@@ -1,7 +1,9 @@
 package me.uwu.saver;
 
+import me.uwu.saver.utils.Emoji;
+
 public class Testing {
     public static void main(String[] args) {
-
+        System.out.println(Emoji.parse("Coucou \uD83D\uDC9C"));
     }
 }
