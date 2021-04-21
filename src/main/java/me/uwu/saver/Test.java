5c56bdfa85072cd3f7a1c30ba6d45a32a61de811
UwU <mmimi0593@gmail.com>
2021-04-21 22:25:05 +0200

    Little bugfix

--- src/main/java/me/uwu/saver/Test.java
+++ src/main/java/me/uwu/saver/Test.java
@@ -1,6 +1,5 @@
 package me.uwu.saver;
 
-import com.google.gson.Gson;
 import me.uwu.saver.objs.Embed;
 
 import java.io.BufferedWriter;
@@ -8,16 +7,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 // 834163332248109118
 
 public class Test {
     public static void main() throws IOException {
-        StringBuilder sb = new StringBuilder();
         StringBuilder html = new StringBuilder();
-
-        Gson gson3 = new Gson();
-        Message[] msg = gson3.fromJson(sb.toString(), Message[].class);
 
         //List<Message> messages = new ArrayList<>(Arrays.asList(msg));
         List<Message> messages = Scrapper.messages;
@@ -54,11 +48,13 @@ public class Test {
             if (message.type == 3) {
                 html.append(Template.endMessageGroup);
                 html.append(Template.addCall(message));
+                maxRep = 99;
             }
 
             if (message.type == 6) {
                 html.append(Template.endMessageGroup);
                 html.append(Template.addPin(message));
+                maxRep = 99;
             }
 
             lastId = message.author.getId();
